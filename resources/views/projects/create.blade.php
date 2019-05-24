eff4173ded05bb8be3ff59bec2aec64827847989
MPesic381 <pesicmiomir@gmail.com>
2019-05-24 15:54:04 +0200

    Lesson 11

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,18 +1,32 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
 @section('content')
-    <form action="{{ route('projects.store') }}" method="post">
-        <div class="form-group">
-            <label for="title">Title:</label>
-            <input class="form-control" type="text" name="title" id="title">
+    <form method="POST" action="/projects">
+        @csrf
+
+        <h1 class="heading is-1">Create a Project</h1>
+
+        <div class="field">
+            <label class="label" for="title">Title</label>
+
+            <div class="control">
+                <input type="text" class="input" name="title" placeholder="Title">
+            </div>
         </div>
-        <div class="form-group">
-            <label for="description">Description:</label>
-            <textarea class="form-control" name="description" id="description"></textarea>
+
+        <div class="field">
+            <label class="label" for="description">Description</label>
+
+            <div class="control">
+                <textarea name="description" class="textarea"></textarea>
+            </div>
         </div>
-        <div class="form-group">
-            <button class="btn btn-primary">Submit</button>
-            <a href="{{ route('projects.index') }}" class="btn btn-secondary">Back</a>
+
+        <div class="field">
+            <div class="control">
+                <button type="submit" class="button is-link">Create Project</button>
+                <a href="/projects">Cancel</a>
+            </div>
         </div>
     </form>
 @endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,13 +1,21 @@
-@extends('layouts.app')
+@extends ('layouts.app')
 
 @section('content')
-    <div style="display: flex; align-items: center">
-        <h1 style="margin-right: auto">Birdboard</h1>
-        <a href="{{ route('projects.create') }}">Create new</a>
-    </div>
-    <ul>
-        @foreach($projects as $project)
-            <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
-        @endforeach
-    </ul>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
+        @forelse ($projects as $project)
+            <div class="lg:w-1/3 px-3 pb-6">
+                @include ('projects.card')
+            </div>
+        @empty
+            <div>No projects yet.</div>
+        @endforelse
+    </main>
 @endsection
